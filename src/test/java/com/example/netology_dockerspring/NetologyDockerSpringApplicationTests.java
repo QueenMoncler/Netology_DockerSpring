8fed2e7c9f45613384436755d48d5078c4474dd7
queen <Queenmoncler@gmail.com>
2023-11-16 22:40:41 +0300

    Initial commit

---
+++ src/test/java/com/example/netology_dockerspring/NetologyDockerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.netology_dockerspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetologyDockerSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
